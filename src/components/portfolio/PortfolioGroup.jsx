54cf2e09a81ecd88b70b7881ae801ca718ee5043
rei_kaji <st.r.z.rei@gmail.com>
2022-10-14 21:44:47 -0700

    fixed eslint alert

--- src/components/portfolio/PortfolioGroup.jsx
+++ src/components/portfolio/PortfolioGroup.jsx
@@ -1,37 +1,37 @@
 const PortfolioGroup = [
   {
     title: "A",
-    image: require("/Users/kajiwararei/Desktop/Development/portfolio-react/src/assets/portfolio1.jpg"),
+    image: require("../../assets/portfolio1.jpg"),
     github: "",
     demo: "",
   },
   {
     title: "B",
-    image: require("/Users/kajiwararei/Desktop/Development/portfolio-react/src/assets/portfolio2.jpg"),
+    image: require("../../assets/portfolio2.jpg"),
     github: "",
     demo: "",
   },
   {
     title: "C",
-    image: require("/Users/kajiwararei/Desktop/Development/portfolio-react/src/assets/portfolio1.jpg"),
+    image: require("../../assets/portfolio1.jpg"),
     github: "",
     demo: "",
   },
   {
     title: "D",
-    image: require("/Users/kajiwararei/Desktop/Development/portfolio-react/src/assets/portfolio1.jpg"),
+    image: require("../../assets/portfolio1.jpg"),
     github: "",
     demo: "",
   },
   {
     title: "E",
-    image: require("/Users/kajiwararei/Desktop/Development/portfolio-react/src/assets/portfolio1.jpg"),
+    image: require("../../assets/portfolio1.jpg"),
     github: "",
     demo: "",
   },
   {
     title: "F",
-    image: require("/Users/kajiwararei/Desktop/Development/portfolio-react/src/assets/portfolio1.jpg"),
+    image: require("../../assets/portfolio1.jpg"),
     github: "",
     demo: "",
   },
