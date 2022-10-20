bd0dc9ecaa6bc80eed679c8c9f87b869e78a67de
rei_kaji <st.r.z.rei@gmail.com>
2022-10-19 19:51:48 -0700

    fixed portfolioDetail page design

--- src/components/portfolio/PortfolioDetail.jsx
+++ src/components/portfolio/PortfolioDetail.jsx
@@ -10,14 +10,7 @@ const PortfolioDetail = () => {
         <h2>{portfolioInfo.title}</h2>
         <div className="portfolioDetail_container">
           <div className="portfolioExplain">
-            <h3>Language & Tool:</h3>
-            <h4>
-              {portfolioInfo.language.map((data) => {
-                return <span key={data}>{data}, </span>;
-              })}
-            </h4>
-            <h3 style={{ marginTop: "2rem" }}>Explain:</h3>
-            <p>{portfolioInfo.explain}</p>
+            <h3>Links:</h3>
             <div className="portfolio_item-cta">
               <a
                 href={portfolioInfo.github}
@@ -36,19 +29,23 @@ const PortfolioDetail = () => {
                 Live Demo
               </a>
             </div>
+            <h3>Language & Tool:</h3>
+            <h4>
+              {portfolioInfo.language.map((data) => {
+                return <span key={data}>{data}, </span>;
+              })}
+            </h4>
+            <h3 style={{ marginTop: "2rem" }}>Explain:</h3>
+            <p>{portfolioInfo.explain}</p>
           </div>
-          <iframe
-            title={portfolioInfo.title}
-            src={portfolioInfo.demo}
-            style={{
-              width: "100%",
-              minHeight: "30rem",
-              maxHeight: "100%",
-              border: "1px solid var(--color-light)",
-            }}
-          ></iframe>
+          <div className="portfolioDetail_iframe">
+            <iframe
+              title={portfolioInfo.title}
+              src={portfolioInfo.demo}
+            ></iframe>
+          </div>
         </div>
-        <div className="link">
+        <div className="portfolioDetail_link">
           <Link
             className="btn btn-primary btn-link"
             to={"/"}
