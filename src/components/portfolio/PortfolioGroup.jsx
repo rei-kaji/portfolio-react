27e772dd7f40c0310b8fd04b048576b86be916f2
rei_kaji <st.r.z.rei@gmail.com>
2023-03-06 20:35:34 -0800

    update portfolio

--- src/components/portfolio/PortfolioGroup.jsx
+++ src/components/portfolio/PortfolioGroup.jsx
@@ -1,6 +1,16 @@
 const FeaturePortfolios = [
   {
     id: 1,
+    title: "3D Portfolio",
+    image: require("../../assets/3dportfolio.png"),
+    github: "https://github.com/rei-kaji/portfolio-3d-threejs.git",
+    demo: "https://rei-portfolio-threejs.netlify.app/",
+    language: ["React", "Three.js", "Tailwind", "HTML", "CSS"],
+    explain:
+      "This portfolio website showcases 3D displays created using Three.js. Users can enjoy viewing PC models and shooting stars that appear to be floating within the website. The website utilizes several advanced techniques, such as elements that dynamically appear as the user scrolls down the page. As the developer, I am excited to present this project as it represents a new level of creativity and technical skill that I have not previously demonstrated. I hope visitors will appreciate the unique visual experience and find inspiration in the implementation of these innovative techniques.",
+  },
+  {
+    id: 2,
     title: "AI Image Generator",
     image: require("../../assets/ai-image-generator.png"),
     github: "https://github.com/rei-kaji/AI-Image-Generator",
@@ -16,25 +26,6 @@ const FeaturePortfolios = [
     ],
     explain:
       "This website is an AI-powered auto-image generator, utilizing cutting-edge technology to create images based on user inputs. The frontend of the website is built using React and Tailwind, while the backend leverages the power of Express and MongoDB. With the integration of OpenAI's Image Generator API, the site generates images in real-time, and with the use of Cloudinary for image storage, it speeds up data retrieval from MongoDB for a seamless user experience.",
-  },
-  {
-    id: 2,
-    title: "Note with GraphCMS",
-    image: require("../../assets/note.png"),
-    github: "https://github.com/rei-kaji/blog_with_MUI-and-GraphCMS",
-    demo: "https://blog-with-mui-and-graphcms.netlify.app",
-    language: [
-      "React",
-      "TypeScript",
-      "MUI",
-      "HTML",
-      "CSS",
-      "Scss",
-      "GraphQL",
-      "GraphCMS",
-    ],
-    explain:
-      "This is an elegantly designed web application that was built using MUI and GraphCMS. It features a user-friendly button that enables users to switch to dark mode and an intuitive modal that showcases a new posting function. This app is optimized for user experience and engagement.",
   },
 ];
 
@@ -67,14 +58,6 @@ const PortfolioGroup = [
       "This Web site! Features include full-scratch overall creation, floating menus, and CSS animations implemented.",
   },
   {
-    title: "Pokemon Picture Book",
-    image: require("../../assets/pokemon.png"),
-    github: "https://github.com/rei-kaji/pokemon-app",
-    demo: "https://pokemon-picture-book.netlify.app/",
-    language: ["React", "pokeAPI", "HTML", "CSS"],
-    explain: "You can check all of Pokemon! It was created with PokeAPI.",
-  },
-  {
     title: "React Native App",
     image: require("../../assets/tesla.png"),
     github: "https://github.com/rei-kaji/react-native-app1",
@@ -92,14 +75,31 @@ const PortfolioGroup = [
     explain:
       "This is an application that allows you to freely create and play with fields and enemies like MarioMaker.",
   },
+  {
+    title: "Note with GraphCMS",
+    image: require("../../assets/note.png"),
+    github: "https://github.com/rei-kaji/blog_with_MUI-and-GraphCMS",
+    demo: "https://blog-with-mui-and-graphcms.netlify.app",
+    language: [
+      "React",
+      "TypeScript",
+      "MUI",
+      "HTML",
+      "CSS",
+      "Scss",
+      "GraphQL",
+      "GraphCMS",
+    ],
+    explain:
+      "This is an elegantly designed web application that was built using MUI and GraphCMS. It features a user-friendly button that enables users to switch to dark mode and an intuitive modal that showcases a new posting function. This app is optimized for user experience and engagement.",
+  },
   // {
-  //   title: "Chat App",
-  //   image: require("../../assets/chat.png"),
-  //   github: "https://github.com/rei-kaji/chat-app",
-  //   demo: "https://rei-chat-app.netlify.app",
-  //   language: ["React", "TypeScript", "Firebase", "MUI", "HTML", "CSS"],
-  //   explain:
-  //     "It was created using the Firebase user authentication system and FireStorage. It can comunicate with your friends on real time.",
+  //   title: "Pokemon Picture Book",
+  //   image: require("../../assets/pokemon.png"),
+  //   github: "https://github.com/rei-kaji/pokemon-app",
+  //   demo: "https://pokemon-picture-book.netlify.app/",
+  //   language: ["React", "pokeAPI", "HTML", "CSS"],
+  //   explain: "You can check all of Pokemon! It was created with PokeAPI.",
   // },
   // {
   //   title: "Film Dictionary",
